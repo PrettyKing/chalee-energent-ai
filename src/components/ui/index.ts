4727b169ff3d3dc4fdca27cffdc611c5a3db8d5e
chalee <36904775+PrettyKing@users.noreply.github.com>
2025-06-15 11:35:09 +0800

    Add UI components index for centralized exports

---
+++ src/components/ui/index.ts
@@ -0,0 +1,9 @@
+// UI Components Library - Centralized exports
+export { default as Button } from './Button';
+export type { ButtonProps } from './Button';
+
+export { default as Input } from './Input';
+export type { InputProps } from './Input';
+
+export { Card, CardHeader, CardContent, CardFooter } from './Card';
+export type { CardProps, CardHeaderProps, CardContentProps, CardFooterProps } from './Card';
